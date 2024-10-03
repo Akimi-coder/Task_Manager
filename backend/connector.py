3b3a0076b89d458425ec5b18bf7e18257ff0240b
Akimi-coder <vladaxl9@gmail.com>
2024-10-03 13:15:47 +0300

    Add backend and database functions

---
+++ backend/connector.py
@@ -0,0 +1,67 @@
+from sqlalchemy import create_engine, Column, Integer, String, Text, Enum, TIMESTAMP, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Task(Base):
+    __tablename__ = 'Tasks'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(255), nullable=False)
+    description = Column(Text)
+    status = Column(Enum('pending', 'in_progress', 'completed'), default='pending')
+    created_at = Column(TIMESTAMP, server_default=func.now())
+    updated_at = Column(TIMESTAMP, server_default=func.now(), onupdate=func.now())
+
+
+class DatabaseConnector:
+    def __init__(self):
+        engine = create_engine('mysql+pymysql://root:root@localhost:3306/db')
+        SessionMaker = sessionmaker(bind=engine)
+        self.session = SessionMaker()
+        try:
+            with engine.connect() as connection:
+                print("Connection successful!")
+        except Exception as e:
+            print("Connection failed:", e)
+
+    def get_tasks(self):
+        tasks = self.session.query(Task).all()
+        return tasks
+
+    def get_task_by_id(self,task_id):
+        task = self.session.query(Task).filter(Task.id == task_id).first()
+        return task
+
+    def delete_task(self,task_id):
+        db_task = self.session.query(Task).filter(Task.id == task_id).first()
+        if db_task is None:
+            return None
+        self.session.delete(db_task)
+        self.session.commit()
+        return db_task
+
+    def add_new_task(self,title, description=None, status='pending'):
+        new_task = Task(title=title, description=description, status=status)
+        self.session.add(new_task)
+        self.session.commit()
+        return new_task
+
+    def update_task(self, task_id, title=None, description=None, status=None):
+        db_task = self.session.query(Task).filter(Task.id == task_id).first()
+        if db_task is None:
+            return None
+
+        if title is not None:
+            db_task.title = title
+        if description is not None:
+            db_task.description = description
+        if status is not None:
+            db_task.status = status
+        if db_task.updated_at is not None:
+            db_task.updated_at = func.now()
+
+        self.session.commit()
+        return db_task
+
--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,51 @@
-from typing import Union
+from enum import Enum
 
-from fastapi import FastAPI
+from pydantic import BaseModel
+
+from connector import DatabaseConnector
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+database = DatabaseConnector()
 
+class Status(str, Enum):
+    pending = 'pending'
+    in_progress = 'in_progress'
+    completed = 'completed'
 
-@app.get("/")
+class TaskBase(BaseModel):
+    title: str
+    description: str
+    status: Status = Status.pending
+
+@app.get("/tasks")
 def read_root():
-    return {"Hello": "World"}
+    tasks = database.get_tasks()
+    return tasks
 
+# Get a task by id
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    task = database.get_task_by_id(task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.delete("/tasks/{task_id}", response_model=dict)
+def delete_task(task_id: int):
+    db_task = database.delete_task(task_id)
+    if db_task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {"detail": "Task deleted"}
+
+@app.post("/tasks/")
+def create_user(task:TaskBase):
+    new_task = database.add_new_task(task.title,task.description,task.status)
+    return new_task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int,task:TaskBase):
+    updated_task = database.update_task(task_id,task.title,task.description,task.status)
+    if updated_task is None:
+        raise HTTPException(status_code=404, detail="Task not found.")
+    return updated_task
